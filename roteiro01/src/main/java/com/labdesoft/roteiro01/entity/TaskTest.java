efa118c847740d245576279f069915e9fbe37ff8
DanielStevam <danielestevam828@gmail.com>
2024-05-16 20:12:49 -0300

    nome

--- roteiro01/src/main/java/com/labdesoft/roteiro01/entity/TaskTest.java
+++ roteiro01/src/main/java/com/labdesoft/roteiro01/entity/TaskTest.java
@@ -1,32 +1,34 @@
-package com.labdesoft.roteiro01.entity;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
-
-public class TaskTest {
-
-    @Test
-    public void testGetterSetter() {
-        // Cria uma instância de Task
-        Task task = new Task();
-
-        // Define os valores dos atributos
-        task.setId(1L);
-        task.setDescription("Exemplo de tarefa");
-        task.setType(TaskType.TYPE_A);
-        task.setDueDate(LocalDate.now());
-        task.setDueDays(3);
-        task.setPriority(Priority.HIGH);
-        task.setStatus(TaskStatus.PENDING);
-
-        // Verifica se os valores atribuídos estão corretos
-        assertEquals(1L, task.getId());
-        assertEquals("Exemplo de tarefa", task.getDescription());
-        assertEquals(TaskType.TYPE_A, task.getType());
-        assertEquals(LocalDate.now(), task.getDueDate());
-        assertEquals(3, task.getDueDays());
-        assertEquals(Priority.HIGH, task.getPriority());
-        assertEquals(TaskStatus.PENDING, task.getStatus());
-    }
-}
+//package com.labdesoft.roteiro01.entity;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.jupiter.api.Test;
+//import org.testng.annotations.Test;
+//
+//import java.time.LocalDate;
+//
+//public class TaskTest {
+//
+//    @Test
+//    public void testGetterSetter() {
+//        // Cria uma instância de Task
+//        Task task = new Task();
+//
+//        // Define os valores dos atributos
+//        task.setId(1L);
+//        task.setDescription("Exemplo de tarefa");
+//        task.setType(TaskType.TYPE_A);
+//        task.setDueDate(LocalDate.now());
+//        task.setDueDays(3);
+//        task.setPriority(Priority.HIGH);
+//        task.setStatus(TaskStatus.PENDING);
+//
+//        // Verifica se os valores atribuídos estão corretos
+//        assertEquals(1L, task.getId());
+//        assertEquals("Exemplo de tarefa", task.getDescription());
+//        assertEquals(TaskType.TYPE_A, task.getType());
+//        assertEquals(LocalDate.now(), task.getDueDate());
+//        assertEquals(3, task.getDueDays());
+//        assertEquals(Priority.HIGH, task.getPriority());
+//        assertEquals(TaskStatus.PENDING, task.getStatus());
+//    }
+//}
