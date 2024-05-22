74f51c45dbdefb86b9b1d7877134b46579855e0c
DanielStevam <danielestevam828@gmail.com>
2024-05-22 11:07:37 -0300

    update

--- roteiro01/src/main/java/com/labdesoft/roteiro01/entity/TaskStatus.java
+++ roteiro01/src/main/java/com/labdesoft/roteiro01/entity/TaskStatus.java
@@ -3,11 +3,7 @@ package com.labdesoft.roteiro01.entity;
 public enum TaskStatus {
     TODO,
     IN_PROGRESS,
-<<<<<<< HEAD
     DONE,
 
     PENDING
-=======
-    DONE, PENDING
->>>>>>> 08809ec168a53e0be46fcf89e8833d90a118deca
 }
--- roteiro01/src/main/java/com/labdesoft/roteiro01/entity/TaskType.java
+++ roteiro01/src/main/java/com/labdesoft/roteiro01/entity/TaskType.java
@@ -3,10 +3,6 @@ package com.labdesoft.roteiro01.entity;
 public enum TaskType {
     FEATURE,
     BUG,
-<<<<<<< HEAD
     IMPROVEMENT,
     TYPE_A
-=======
-    IMPROVEMENT, TYPE_A
->>>>>>> 08809ec168a53e0be46fcf89e8833d90a118deca
 }
